8f384c9a999e757f1edf518dd7c34576d461818a
snehabwp27 <sneha.bwp27@gmail.com>
2020-12-02 15:44:58 -0800

    Lab 3

--- Lab1/app/src/main/java/com/ppbnr/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/ppbnr/lab1/MainActivity.kt
@@ -2,12 +2,15 @@ package com.ppbnr.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +32,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -39,10 +44,12 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
+            toggleButtonState(false)
         }
 
         falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
+            toggleButtonState(false)
         }
 
         questionTextView.setOnClickListener {
@@ -58,6 +65,37 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    private fun toggleButtonState(toggle: Boolean) {
+        trueButton.isClickable = toggle
+        trueButton.isEnabled = toggle
+        falseButton.isClickable = toggle
+        falseButton.isEnabled = toggle
+    }
     private fun nextQuestion() {
         currentIndex = (currentIndex + 1) % questionBank.size
         updateQuestion()
@@ -69,11 +107,13 @@ class MainActivity : AppCompatActivity() {
         }
         currentIndex = (currentIndex - 1) % questionBank.size
         updateQuestion()
+        toggleButtonState(false)
     }
 
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+        toggleButtonState(true)
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
